d2a7cf187a992be5ded2cf2538768cb0f4b87785
Sumer   Gaikwad <sg.comparch@gmail.com>
2021-01-07 08:21:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
     LEFT OUTER JOIN pledges
     ON projects.id = pledges.project_id
     GROUP BY projects.title
+<<<<<<< HEAD
     HAVING projects.funding_goal <= SUM(pledges.amount);"
+=======
+    WHERE projects.funding_goal <= pledges.amount;"
+>>>>>>> f51e000a2fcc003607c73e6f42df097e7730ffad
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
